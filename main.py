f9c87321c220d9aa1f15cb9e660a45c8d14e47e6
Дмитрий Поздеев <dmitrypozdeyev@yandex.ru>
2025-02-20 17:33:37 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import pygame
+
+from objectts import *
+
+cl = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pl = Player(screen)
+
+preps.add(Prep(screen, pl.bullets, 350, 250))
+
+
+def check_events():
+    global run
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                pl.start_move_forw()
+            elif event.key == pygame.K_DOWN:
+                pl.start_move_back()
+            elif event.key == pygame.K_LEFT:
+                pl.start_move_left()
+            elif event.key == pygame.K_RIGHT:
+                pl.start_move_right()
+            elif event.key == pygame.K_SPACE:
+                pl.shoot()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                pl.stop_move_right()
+            elif event.key == pygame.K_LEFT:
+                pl.stop_move_left()
+            elif event.key == pygame.K_UP:
+                pl.stop_move_forw()
+            elif event.key == pygame.K_DOWN:
+                pl.stop_move_back()
+                
+    
+
+def main():
+    global run
+    run = True
+    while run:
+        screen.fill((0, 0, 0))
+        check_events()
+        pl.bullets.update()
+        pl.update()
+        preps.update()
+        pygame.display.update()
+        cl.tick(200)
+        
+                
+if __name__ == "__main__":
+    main()
---
+++ objectts.py
@@ -0,0 +1,146 @@
+import pygame
+preps = pygame.sprite.Group()
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y, pl):
+        super().__init__()
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(self.x-5, self.y-5, 10, 10)
+        self.pl = pl
+        self.direction = pl.direction
+    
+    def update(self):
+        self.rect = pygame.Rect(self.x-5, self.y-5, 10, 10)
+        pygame.draw.circle(self.screen, (219, 160, 36), (self.x, self.y), 5)
+        if self.direction == 'right':
+            self.x += 5
+        elif self.direction == 'left':
+            self.x -= 5
+        elif self.direction == 'forw':
+            self.y -= 5
+        elif self.direction == 'back':
+            self.y += 5
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, screen, x = 0, y = 0):
+        super().__init__()
+        self.image = pygame.image.load("files/playerforw.png")
+        self.rect = self.image.get_rect(topleft = (x, y))
+        self.x = x
+        self.y = y
+        self.screen = screen
+        self.move_right = False
+        self.move_left = False
+        self.move_forw = False
+        self.move_back = False
+        self.bullets = pygame.sprite.Group()
+        self.direction = 'forw'
+        
+    def rot_right(self):
+        self.image = pygame.image.load("files/playerright.png")
+        self.direction = 'right'
+        
+    def rot_left(self):
+        self.image = pygame.image.load("files/playerleft.png")
+        self.direction = 'left'
+    
+    def rot_forw(self):
+        self.image = pygame.image.load("files/playerforw.png")
+        self.direction = 'forw'
+    
+    def rot_back(self):
+        self.image = pygame.image.load("files/playerback.png")
+        self.direction = 'back'
+    
+    def start_move_right(self):
+        self.rot_right()
+        self.move_right = True
+
+    def stop_move_right(self):
+        self.move_right = False
+        
+    def start_move_left(self):
+        self.rot_left()
+        self.move_left = True
+
+    def stop_move_left(self):
+        self.move_left = False
+        
+    
+    def start_move_forw(self):
+        self.rot_forw()
+        self.move_forw = True
+
+    def stop_move_forw(self):
+        self.move_forw = False
+        
+    def start_move_back(self):
+        self.rot_back()
+        self.move_back = True
+
+    def stop_move_back(self):
+        self.move_back = False
+        
+    def shoot(self):
+        self.bullets.add(Bullet(self.screen, self.x + 50, self.y + 50, self))
+        
+       
+    
+    
+            
+    def update(self):
+        self.screen.blit(self.image, (self.x, self.y))
+        if self.move_right: 
+            self.x += 1
+        elif self.move_left:
+            self.x -= 1
+        elif self.move_forw: 
+            self.y -= 1
+        elif self.move_back:
+            self.y += 1
+        self.rect = self.image.get_rect(topleft = (self.x, self.y))
+        for prep in preps:
+            if self.rect.colliderect(prep.rect):
+                self.move_forw = False
+                self.move_back = False
+                self.move_left = False
+                self.move_right = False
+        
+        
+class Prep(pygame.sprite.Sprite):
+    def __init__(self, screen, bullets, x = 0, y = 0):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.screen = screen
+        self.health = 3
+        self.image = pygame.image.load('files/prep3.png')
+        self.rect = self.image.get_rect(topleft = (x, y))
+        self.bullets = bullets
+    
+    
+        
+    
+    def update(self):
+        self.screen.blit(self.image, (self.x, self.y))
+        self.rect = self.image.get_rect(topleft = (self.x, self.y))
+        
+        for bullet in self.bullets:
+            if self.rect.colliderect(bullet.rect):
+                self.health -= 1
+                bullet.kill()
+        
+        if self.health == 3:
+            self.image = pygame.image.load('files/prep3.png')
+        elif self.health == 2:
+            self.image = pygame.image.load('files/prep2.png')
+        elif self.health == 1:
+            self.image = pygame.image.load('files/prep1.png')
+        elif self.health == 0:
+            self.kill()
+            
+    
+                
